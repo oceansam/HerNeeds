eb710d360fe1741261685adf46e716282966e948
Kara Huynh <karahuynh122@gmail.com>
2022-05-21 23:38:31 -0400

    fixing paddings and margins

--- quasar/src/pages/LandingPage.vue
+++ quasar/src/pages/LandingPage.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="row landing-container">
+    <div class="row landing-container q-pl-xl">
         <div class="landing-body col q-pl-xl">
             <div class="text-h2 q-pb-sm" style="font-weight:600;" >find affordable resources.</div>
-            <div class="text-h2" style="font-weight:600;">always be prepared with</div>
+            <div class="text-h2 q-pb-sm" style="font-weight:600;">always be prepared with</div>
             <div class="text-h2" style="font-weight:600;">her needs.</div>
-            <br>
+            <br><br>
     
             <q-btn class="landing-button" filled color="primary" text-color="black" size="xl" label="Find Products" />
-            <q-btn class="landing-button" filled color="black" text-color="white" size="xl" label="Find Locations" />
+            <q-btn class="landing-button q-ml-xl" filled color="black" text-color="white" size="xl" label="Find Locations" />
          
         </div>
-        <div class="col gt-sm" id="image" >
+        <div class="col gt-sm q-pl-xl" id="image" >
             <img src="https://cdn.discordapp.com/attachments/977624874992410724/977723087221055548/Cool_Kids_-_Brainstorming.png" width="550"/>
         </div>
         <svg id="wave1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
@@ -23,6 +23,7 @@
 <style scoped>
     .landing-container{
         margin-top: 15%;
+        width: 100%;
     }
 
     #wave1{
